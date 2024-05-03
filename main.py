50c77709c51f8ed0d1dd34c3379487388ff5a25d
Zalexanninev15 <blue.shark@disroot.org>
2024-05-03 13:18:59 +0300

    Arch-EIP v1.4-dev1
    Changes:
    - Added the ability to export packages installed using the dnf package manager on Fedora OS
    - Added export as a bash script for quick installation
    - Now not everything is exported at once, but there is a menu with the ability to choose from which batch managers to export and in which format (bash script or just a text file)
    - Improved export of PIP packages (Python)
    - Added export of packages installed by the Cargo package manager (Rust) to the plans
    - Added more information that explains the points related to what project/product this package manager is intended for
    - Improved output of information in the terminal

--- main.py
+++ main.py
@@ -17,48 +17,102 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import subprocess
+import os
 
-def write_to_file(commands, file):
-    with open(f"{file}.txt", "w") as f:
+
+def write_to_file(commands, file, is_bash):
+    if (is_bash):
+        ext = 'sh'
+    else:
+        ext = 'txt'
+    with open(f"{file}.{ext}", "w") as f:
         subprocess.run(commands, shell=True, stdout=f, text=True)
+    if (os.stat(f"{file}.{ext}").st_size == 0):
+        os.remove(f"{file}.{ext}")
+        print('Status: Recording error, because no text for recording was found!')
+    else:
+        print('Status: Export completed successfully!')
+
 
 print('''Arch-EIP  Copyright (C) 2023-2024  Zalexanninev15
 This program comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it
 under certain conditions.''')
 
-print("\nArch-EIP v1.4-dev0.1 (Flatpak+AUR+Official+PIP+Cargo+Fedora DNF) by Zalexanninev15\nGitHub: https://github.com/Zalexanninev15/Arch-EIP\n")
-
-# Step 1.
 # Choosing what to export
+print("\nArch-EIP v1.4-dev1 (Flatpak+AUR+Official+PIP+Cargo+DNF) by Zalexanninev15\nGitHub: https://github.com/Zalexanninev15/Arch-EIP\n")
 
-# Step 2.
-# File format selection: a list (.txt) or a script for quick installation (.sh)
+# Step 1. Choosing what to export. All or choose many
+print("Step 1. Choose what to export:")
+print("1. All")
+print("2. Choose individually")
+choice = input("Enter your choice (1 or 2): ")
 
-print("[*] Working (4 steps)...\n")
+if choice == "1":
+    export_all = True
+else:
+    export_all = False
+    export_choices = []
 
-print('[!] Step 1. Flatpak')
-flatpak = 'flatpak list --app --columns=name --columns=application'
-write_to_file(flatpak, "Flatpak")
+    print("\nSelect what to export.")
+    print("1. Flatpak")
+    print("2. AUR (Arch Linux)")
+    print("3. Official (Arch Linux)")
+    print("4. PIP (Python 3)")
+    print("5. Cargo (Rust)")
+    print("6. DNF (Fedora)")
 
-print('[!] Step 2. AUR')
-aur = 'pamac list --foreign'
-write_to_file(aur, "AUR")
+    while True:
+        choice = input("\nEnter the corresponding number (one at a time, then you need to press Enter and enter the next one) or 'q' to finish: ")
+        if choice == "q":
+            break
+        elif choice in ["1", "2", "3", "4", "5", "6"]:
+            export_choices.append(int(choice))
+        else:
 
-print('[!] Step 3. Official')
-official = 'pamac list --installed | grep -v AUR'
-write_to_file(official, "Official")
+# Step 2. File format selection: a list (.txt) or a script for quick installation (.sh)
+format_bash = input(
+    "\nDo you want to generate a script for quick installation (.sh)? (y/n): ").lower() == "y"
 
-print('[!] Step 4. PIP (Python 3)')
-pip3 = 'pip3 list'
-write_to_file(pip3, "PIP")
+print("")
 
-print('[!] Step 5. Cargo')
-cargo = ''
-write_to_file(cargo, "Cargo")
+if export_all or 1 in export_choices:
+    print('- Flatpak')
+    if (format_bash):
+        flatpak = "echo '#!/bin/bash'\"\nflatpak install \"$(flatpak list --app --columns=application | xargs echo -n)"
+    else:
+        flatpak = 'flatpak list --app --columns=name --columns=application'
+    write_to_file(flatpak, "Flatpak", format_bash)
+    
+if export_all or 2 in export_choices:
+    print('- AUR (Arch Linux) [ONLY TXT]')
+    aur = 'pamac list --foreign'
+    write_to_file(aur, "AUR", format_bash)
 
-print('[!] Step 6. Fedora DNF')
-dnf = ''
-write_to_file(dnf, "Fedora DNF")
+if export_all or 3 in export_choices:
+    print('- Official (Arch Linux) [ONLY TXT]')
+    official = 'pamac list --installed | grep -v AUR'
+    write_to_file(official, "Official", format_bash)
 
-print('\n[+] Done! All installed packages are written in the files!')
+if export_all or 4 in export_choices:
+    print('- Step 4. PIP (Python 3)')
+    if (format_bash):
+        pip3 = "echo '#!/bin/bash'\"\npip install \"$(pip list --format freeze | sed 's/==.*//' | xargs echo -n)"
+    else:
+        pip3 = 'pip3 list --format=columns'
+    write_to_file(pip3, "PIP", format_bash)
+
+if export_all or 5 in export_choices:
+    print('- Cargo (Rust) [In developing!]')
+    cargo = ''
+    write_to_file(cargo, "Cargo", format_bash)
+
+if export_all or 6 in export_choices:
+    print('- DNF (Fedora)')
+    if (format_bash):
+        dnf = "echo '#!/bin/bash'\"\nsudo dnf install \"$(rpm --query --all --queryformat '%{NAME} ' | xargs echo -n)"
+    else:
+        dnf = "rpm --query --all --queryformat '%{NAME}[%{ARCH}] %{VERSION} — %{SUMMARY}\n'"
+    write_to_file(dnf, "Fedora DNF", format_bash)
+
+print('\nDone! All installed packages are written in the files!')
